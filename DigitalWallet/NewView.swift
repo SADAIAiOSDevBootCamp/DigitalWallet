1d984f3542f7c8d5a0d5c86164d5fdb936566b00
Dhai Alassaf <dhasaf@gmail.com>
2024-03-05 17:44:28 +0300

    New View

---
+++ DigitalWallet/NewView.swift
@@ -0,0 +1,18 @@
+//
+//  NewView.swift
+//  DigitalWallet
+//
+//  Created by Dhai Alassaf on 24/08/1445 AH.
+//
+
+import SwiftUI
+
+struct NewView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewView()
+}
