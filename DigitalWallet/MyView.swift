85d62544e0f93316a9d6a91ec3e8d81f070a1c69
Nouf Faisal <nouf@alMacBook-Air-alkhas-bNouf.local>
2024-03-05 17:47:36 +0300

    add new file
    I add new view

---
+++ DigitalWallet/MyView.swift
@@ -0,0 +1,18 @@
+//
+//  MyView.swift
+//  DigitalWallet
+//
+//  Created by Nouf Faisal  on 24/08/1445 AH.
+//
+
+import SwiftUI
+
+struct MyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyView()
+}
