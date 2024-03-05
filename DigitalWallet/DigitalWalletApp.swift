6cc4e6ef60d6cd062e3130f5b2dfc7e4440e09de
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2024-03-05 16:43:32 +0300

    Initial Commit

---
+++ DigitalWallet/DigitalWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalWalletApp.swift
+//  DigitalWallet
+//
+//  Created by Tariq Almazyad on 05/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct DigitalWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
