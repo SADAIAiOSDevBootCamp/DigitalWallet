ee4d43d5b64d013403acb57b6f63d176dbe1c0de
Atheer <atheeraf20@gmail.com>
2024-03-05 18:40:55 +0300

    UI

---
+++ DigitalWallet/AtheerView.swift
@@ -0,0 +1,18 @@
+//
+//  AtheerView.swift
+//  DigitalWallet
+//
+//  Created by أثير on 24/08/1445 AH.
+//
+
+import SwiftUI
+
+struct AtheerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AtheerView()
+}
