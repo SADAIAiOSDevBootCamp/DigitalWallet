0b11e00a624e93acb55daaf0142d8b5795a2f155
Dhai Alassaf <dhasaf@gmail.com>
2024-03-05 20:27:22 +0300

    New changes 2

---
+++ DigitalWallet/CreatingNewFile.swift
@@ -0,0 +1,19 @@
+//
+//  CreatingNewFile.swift
+//  DigitalWallet
+//
+//  Created by Dhai Alassaf on 24/08/1445 AH.
+//
+
+import SwiftUI
+
+struct CreatingNewFile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hi")
+    }
+}
+
+#Preview {
+    CreatingNewFile()
+}
