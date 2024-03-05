c9280f6c85cf950257d79eb951c2f2b4b80c5e64
Sarah <sarhsalkhamees@gmail.com>
2024-03-05 19:15:13 +0300

    Add new code.

---
+++ DigitalWallet/SarahSwiftUI.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.blue // Background color
+            
+            Text("Hello, World!")
+                .font(.largeTitle)
+                .foregroundColor(.white)
+            
+            Circle()
+                .fill(Color.red)
+                .frame(width: 100, height: 100)
+            
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 200, height: 150)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
