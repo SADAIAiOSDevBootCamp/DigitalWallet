249dc61aa08b3b02d1deaf688361026f2f583ae1
Raneem <83226325+Raneem-pro@users.noreply.github.com>
2024-03-05 19:26:56 +0300

    new_f

---
+++ DigitalWallet/Raneem.swift
@@ -0,0 +1,18 @@
+//
+//  Raneem.swift
+//  DigitalWallet
+//
+//  Created by رنيم القرني on 24/08/1445 AH.
+//
+
+import SwiftUI
+
+struct Raneem: View {
+    var body: some View {
+        Text("new feature")
+    }
+}
+
+#Preview {
+    Raneem()
+}
