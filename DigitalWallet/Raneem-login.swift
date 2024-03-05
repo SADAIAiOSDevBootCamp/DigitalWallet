79304e69fbf102e65d1f49db2c40a85ce24e59b9
Raneem <83226325+Raneem-pro@users.noreply.github.com>
2024-03-05 17:47:57 +0300

    Raneem-login

---
+++ DigitalWallet/Raneem-login.swift
@@ -0,0 +1,18 @@
+//
+//  Raneem-login.swift
+//  DigitalWallet
+//
+//  Created by رنيم القرني on 24/08/1445 AH.
+//
+
+import SwiftUI
+
+struct Raneem_login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Raneem_login()
+}
