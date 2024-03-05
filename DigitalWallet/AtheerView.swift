8a939b325d606e2469115e56dae8797c5e0cea0f
Atheer <atheeraf20@gmail.com>
2024-03-05 19:03:56 +0300

    add new file Atheer

--- DigitalWallet/AtheerView.swift
+++ DigitalWallet/AtheerView.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct AtheerView: View {
+    @State private var name: String = "Atheer"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer() 
+            TextField("Name", text: $name)
+                .padding()
+                .font(.system(size: 24))
+                .padding(EdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20))
+            Spacer()
+        }
     }
 }
 
-#Preview {
-    AtheerView()
+struct AtheerView_Previews: PreviewProvider {
+    static var previews: some View {
+        AtheerView()
+    }
 }
+
