95399eccc8b4d0e46823e123bd3d9d8930094912
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2024-03-07 17:54:22 +0300

    Added nav view

--- DigitalWallet/AlmazyadView.swift
+++ DigitalWallet/AlmazyadView.swift
@@ -9,10 +9,14 @@ import SwiftUI
 
 struct AlmazyadView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear{
-                print("")
+        NavigationStack {
+            List {
+                Text("Hello, World!")
+                    .onAppear{
+                        print("")
+                    }
             }
+        }
     }
 }
 
